6c6cadef84fe26d548feb900b683d230c97e2a2d
Gathoni Wanjira <janetgathoni910@gmail.com>
2023-07-26 23:45:24 +0300

    Assignment Done

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,22 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
 
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  return <div>
+    <NavLink>
+                <Link to = '/'>Home</Link>
+    </NavLink>
+    <NavLink to="/movies" >
+        Movies
+      </NavLink>
+      <NavLink to="/directors" >
+        Directors
+      </NavLink>
+      <NavLink to="/actors" >
+        Actors
+      </NavLink>
+  </div>;
 }
 
 export default NavBar;
